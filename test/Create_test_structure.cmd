18872a243d1c412c11a0ae38da8f2fbf66ddbf21
SKruch <skruch00@gmail.com>
2025-01-29 18:50:51 +0200

    доопрацьовано скрипти для створення файлів тестової структури

--- test/Create_test_structure.cmd
+++ test/Create_test_structure.cmd
@@ -19,37 +19,5 @@ FOR %%G IN (Empty_Dir, Empty_SubDir\Empty_Dir ) DO (
   @echo FOLDER: %Root%\test\_%%G
   if not exist %Root%\test\_%%G (mkdir %Root%\test\_%%G)
 )
-
-
-
 popd
 exit 0
-
-:setfiletime
-
-
-mkdir %Root%\test\_DIR_new\_SubDir
-mkdir %Root%\test\_DIR_old_3Day\_SubDir
-mkdir %Root%\test\_DIR_old_7Day\_SubDir
-mkdir %Root%\test\_DIR_old_10Day\_SubDir
-mkdir %Root%\test\Empty_Dir
-mkdir %Root%\test\Empty_SubDir\Empty_Dir
-
-echo test >%Root%\test\_DIR_new\file_new.txt
-echo test >%Root%\test\_DIR_new\file_old_7Day.txt
-echo test >%Root%\test\_DIR_new\file_old_10Day.txt
-echo test >%Root%\test\_DIR_new\file_old_300Day.txt
-
-echo test >%Root%\test\_DIR_new\_SubDir\file_new.txt
-echo test >%Root%\test\_DIR_new\_SubDir\file_old_7Day.txt
-echo test >%Root%\test\_DIR_new\_SubDir\file_old_10Day.txt
-echo test >%Root%\test\_DIR_new\_SubDir\file_old_300Day.txt
-
-
-echo test >%Root%\test\_DIR_new\file_new.txt
-echo test >%Root%\test\_DIR_\file_old_7Day.txt
-echo test >%Root%\test\_DIR_new\fileold_10Day.txt
-echo test >%Root%\test\_DIR_new\fileold_300Day.txt
-
-
-
